8c9f4d4638b305b41b457131c50738e2e69c8252
Alexander Volz <Alexander@volzit.de>
2023-05-11 23:54:46 +0200

    test

--- roles/kde-desktop-customisation/templates/configure_plasma_shell.sh
+++ roles/kde-desktop-customisation/templates/configure_plasma_shell.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo "executed on $(date)" 
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "$(</home/{{ desktop_username }}/.config/ansible_distro/configure_plasma_shell.js)"
+# qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "$(</home/{{ desktop_username }}/.config/ansible_distro/configure_plasma_shell.js)"
